5aa3c06c960b2485351404b1fcacb5adc07e50c7
Mxolisi Fikeni <mlfikeni@gmail.com>
2024-05-15 12:18:52 +0200

    Updated Models project.

---
+++ Recruitment.Models/DTOs/ApplicationDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Recruitment.Models.DTOs
+{
+    public class ApplicationDTO
+    {
+        public Guid EmploymentProgramId { get; set; }
+        public Candidate Candidate { get; set; }
+        public List<AnswerDTO> Answers { get; set; }
+    }
+}
