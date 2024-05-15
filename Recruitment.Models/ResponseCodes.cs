c6c91229e9c19d2510d6b276ff0233e66d5e07b0
Mxolisi Fikeni <mlfikeni@gmail.com>
2024-05-15 12:46:20 +0200

    Added initial Services project.

---
+++ Recruitment.Models/ResponseCodes.cs
@@ -0,0 +1,12 @@
+﻿namespace Recruitment.Models
+{
+    public static class ResponseCodes
+    {
+        public const int Ok = 200;
+        public const int NoContent = 204;
+        public const int BadRequest = 400;
+        public const int Unauthorized = 401;
+        public const int NotFound = 404;
+        public const int InternalServerError = 500;
+    }
+}
